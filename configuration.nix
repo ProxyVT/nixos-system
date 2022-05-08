0c75e9581bd4f8e35890a8d438b47e3313e68497
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-05-08 18:34:33 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,263 +2,210 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+	{ config, pkgs, ... }:
+	
+	let
+  		unstableTarball =
+    		fetchTarball
+    			https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+	in
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+	{
+	imports =
+	[ # Include the results of the hardware scan.
+		./hardware-configuration.nix
+	];
    
 
-  # Use the systemd-boot EFI boot loader.
-  boot.kernelPackages = pkgs.linuxPackages_5_15;
-  boot.supportedFilesystems = [ "ntfs" ];
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.timeout = 10;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2; 
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.default = 2;
-  boot.loader.grub.configurationLimit = 50;
-  boot.tmpOnTmpfs = true;
-
-  networking.hostName = "powehi"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+# System boot section
   
+	boot = {
+	
+		# Linux kernel version
+		kernelPackages = pkgs.linuxPackages_5_15;
+		 
+		# Supported file systems
+	  	supportedFilesystems = [ "ntfs" ];
+	  	
+	  	# Tmp path
+	  	tmpOnTmpfs = true;
+	  	
+  		loader = {
+	  		efi.canTouchEfiVariables = true;
+	  		efi.efiSysMountPoint = "/boot/efi";
+	  		timeout = 10;
+  		 
+	  		grub = {
+	  			enable = true;
+	  			version = 2; 
+	  			device = "nodev";
+	  			efiSupport = true;
+	  			useOSProber = true;
+	  			default = 2;
+	  			configurationLimit = 50;
+	  			splashImage = "/boot/efi/cat.png";
+	  		};	
+	  	};
+	};
 
-  # Set your time zone.
-  time = {
-	timeZone = "Europe/Istanbul";
-	hardwareClockInLocalTime = true;
-  };
+# Define your hostname.
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp5s0.useDHCP = true;
+	networking = {
+		hostName = "ulad";
+		interfaces.enp2s0.useDHCP = true;
+		#wireless.enable = true; 
+	};
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# Set your time zone.
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
+  	time = {
+  		timeZone = "Europe/Istanbul";
+  		hardwareClockInLocalTime = true;
+  	};
 
-  # Enable the X11 windowing system and Pantheon Desktop Environment.
-  services.xserver.enable = true;
-  services.xserver.desktopManager.pantheon.enable = true;
-  services.xserver.exportConfiguration = true;
- 
-
-  # Configure keymap in X11
-  services.xserver.layout = "us, ru";
-# services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-#  services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+# Select internationalisation properties.
   
-  users.users.alvaro = {
-	isNormalUser  = true;
-	home  = "/home/alvaro";
-	description  = "Alvaro";
-	extraGroups  = [ "wheel" "networkmanager" "adbusers" ];
-  };
+	i18n.defaultLocale = "en_US.UTF-8";
+	console = {
+		font = "Lat2-Terminus16";
+		keyMap = "us";
+	};
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+# Global services configuration
   
-  programs = {
-  	adb.enable = true;
+	services = {  
+		# Environment configuration	  
+		xserver = {
+	  		enable = true;
+	  		desktopManager.pantheon.enable = true;
+	  		videoDrivers = [ "nvidia" ];
+	  		layout = "us,ru";
+	  		libinput.enable = true; # Touchpad support
+	  	};
+	  	
+	  	# Sound services configuration
+	  	pipewire = {
+    		enable = true;
+    		alsa = {
+    			enable = true;
+    			support32Bit = true;
+    		};
+    	pulse.enable = true;
+  		};
+  		
+  		# Printing services
+	  	printing.enable = true;
+	  	
+	  	# Programms services
+		deluge.enable = true;
+		pantheon.apps.enable = false;
+		mullvad-vpn.enable = true;
+		aria2.enable = true;
+		
+		# Enable the OpenSSH daemon.
+		openssh.enable = true;
+		
+		#BTRFS autoScrub
+		btrfs.autoScrub.enable = true;  	
+	};
+	
+# Global hardware configuration
+
+	hardware = {
+		# Nvidia package
+		nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+		
+		# Steam + Opengl
+		steam-hardware.enable = true;
+		opengl.enable = true;
+		opengl.driSupport = true;
+		opengl.driSupport32Bit = true;
+		
+		# Pulseaudio hardware acces
+		pulseaudio.enable = false;	
+	};
+
+# Enable sound.
+	sound.enable = true;
+	security.rtkit.enable = true;
+
+# Define a user account. Don't forget to set a password with ‘passwd’.
+  
+	users.users.amerigo = {
+		isNormalUser  = true;
+		home  = "/home/amerigo";
+		description  = "Amerigo";
+		extraGroups  = [ "wheel" "adbusers" ];
+  	};
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+  
+	programs = {
+    	adb.enable = true;
     	gnome-disks.enable = true;
     	pantheon-tweaks.enable = true;
     	partition-manager.enable = true;
     	steam.enable = true;
-    	java.enable = true;
     	git.enable = true;
-  };
+  	};
   
-  nixpkgs.config = {
-	allowUnfree = true;
-	packageOverrides = pkgs: {
-    unstable = import <nixpkgs-unstable> {
-	  config = config.nixpkgs.config;
-	  };
+	nixpkgs.config = {
+		allowUnfree = true;
+		packageOverrides = pkgs: {
+			unstable = import unstableTarball {
+			config = config.nixpkgs.config;
+			};
+		};  
 	};
-  };
+	
+	nix.autoOptimiseStore = true;
+	nix.readOnlyStore = false;
   
+   
   environment.systemPackages = with pkgs; [
-
+  
+  # Stable apps
+  
 	appimage-run
 	baobab
-	clamav
+	compsize
 	deluge
+	easyeffects
 	eclipses.eclipse-java
 	far2l
-	geany
 	github-desktop
-	gammy
-	gcolor2
 	goverlay
 	gpick
 	guake
 	hddtemp
+	hdparm
 	htop
-	hwinfo
-	jdk8
 	keeweb
+	krita
 	libreoffice-fresh
 	lm_sensors
-	mangohud
-	minecraft
+	lxqt.pcmanfm-qt
 	monitor
-	mousetweaks
 	mullvad-vpn
 	neofetch
 	pavucontrol
 	pantheon.elementary-files
 	pantheon.elementary-screenshot
-	python39Packages.secretstorage
-	qdirstat
-	qimgv
-	qrcp
-	s-tui
-	testdisk-qt
-	tilix
-	ventoy-bin
-	vlc
-	wget
-    
-	unstable.scrcpy
-	unstable.yt-dlp
-	unstable.pinta
-	unstable.krita
-	unstable.mkvtoolnix
-	unstable.vivaldi
-	unstable.handbrake
-	unstable.tdesktop
-	unstable.mpv
-	unstable.olive-editor
-	unstable.cudatext-gtk
-  ];
-  
-  fonts.fonts = with pkgs; [
-	comic-relief
-	ibm-plex
-	jost
-  ];
-  
-  services = {
-	deluge.enable = true;
-	mullvad-vpn.enable = true;
-	pantheon.apps.enable = false;
-	aria2.enable = true;
-    clamav = {
-      daemon.enable = true;
-      updater.enable = true;
-    };
-  };
-  
-  hardware = {
-	opengl.enable = true;
-	opengl.driSupport = true;
-	opengl.driSupport32Bit= true;	
-  }; 
-  
-  
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
-  
-  # -----Windows 7 Specific Settings-----
-
- boot.loader.grub.splashImage = "/boot/efi/cat.png";
- 
- users.users.amerigo = {
-	isNormalUser  = true;
-	home  = "/home/amerigo";
-	description  = "Amerigo";
-	extraGroups  = [ "wheel" "networkmanager" "adbusers" ];
-  };
-  
-    environment.systemPackages = with pkgs; [
-  
-	appimage-run
-	baobab
-	clamav
-	deluge
-	eclipses.eclipse-java
-	far2l
-	github-desktop
-	goverlay
-	gpick
-	guake
-	hddtemp
-	htop
-	keeweb
-	libreoffice-fresh
-	lm_sensors
-	monitor
-	mullvad-vpn
-	neofetch
-	pavucontrol
-	pantheon.elementary-screenshot
 	pantheon.epiphany
-	qdirstat
+	pantheon.file-roller
 	qimgv
 	qrcp
 	s-tui
-	spaceFM
 	testdisk-qt
-	vlc
 	xclip
 	wget
     
+    # Unstable apps
+    
 	unstable.scrcpy
 	unstable.yt-dlp
 	unstable.pinta
-	unstable.krita
 	unstable.mkvtoolnix
 	unstable.vivaldi
 	unstable.handbrake
@@ -275,16 +222,21 @@
 	jost
   ];
   
-  services = {
-	deluge.enable = true;
-	pantheon.apps.enable = false;
-	mullvad-vpn.enable = true;
-	aria2.enable = true;
-	clamav = {
-      daemon.enable = true;
-      updater.enable = true;
-    };
-  }; 
- 
-}
+  
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  
 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+}
