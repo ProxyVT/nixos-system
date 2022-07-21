e6d5cd947310eff0ed8e69f8bcae103609f588f6
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-07-21 22:30:04 +0300

    Create environment.nix

---
+++ environment.nix
@@ -0,0 +1,66 @@
+  	environment.systemPackages = with pkgs; [
+  
+  	# Stable apps
+  	alacritty
+		appimage-run
+		audacity
+		bastet
+		bpytop
+		cloudflare-warp
+		compsize
+		cudatext-gtk
+		deluge
+		easyeffects
+		eclipses.eclipse-java
+		far2l
+		github-desktop
+		glances
+		gpick
+		handbrake
+		htop
+		keeweb
+		krusader
+		libreoffice-fresh
+		libsForQt5.ark
+		lm_sensors
+		lutris	
+		mpv
+		mullvad-vpn
+		neofetch
+		openvpn
+		openssl
+		onlyoffice-bin
+		pinta
+		psensor
+		python39Packages.secretstorage
+		qdirstat
+		qimgv
+		qrcp
+		s-tui
+		tdesktop
+		testdisk-qt
+		transmission-qt
+		xclip
+		xournalpp
+		xsensors
+		wget2
+		
+		# Games
+		playonlinux
+		unstable.mangohud
+		unstable.goverlay
+		unstable.vkBasalt
+		unstable.replay-sorcery
+    
+    	# Unstable apps
+		unstable.scrcpy
+		unstable.yt-dlp
+		unstable.mkvtoolnix
+		unstable.vivaldi
+		unstable.ventoy-bin
+		unstable.krita
+		unstable.libsForQt5.kdenlive
+		
+		# Plasma tilling
+		libsForQt5.bismuth
+  	];
