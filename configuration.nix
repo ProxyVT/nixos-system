fe2eec67d23384714b233b89508406e9c8b3381e
ProxyVT <tikit.us@outlook.com>
2023-08-14 20:16:29 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -178,153 +178,49 @@ users = {
 };
 
 home-manager.users.ulad = {
-        home = {
-                packages = with pkgs; [
-                
-                # Development
-                super-productivity
-                android-studio
-                cudatext
-                lite-xl
-                lapce
-                kate
-                eclipses.eclipse-java
-                github-desktop
-                nodejs
-                nodePackages.gulp
-                
-                # Graphics
-                gpick
-                krita
-                pinta
-                qimgv
-                blanket
-                
-                # Internet
-                aria2
-                vivaldi
-                vivaldi-ffmpeg-codecs
-                you-get
-                browsh
-                cloudflare-warp
-                firefox
-                tdesktop
-                motrix
-                streamlink
-                deluge
-								ktorrent
-                                        
-                # Server & security
-                keepassxc
-                john
-                putty
-                
-                # Multimedia
-                audacity
-                handbrake
-                libsForQt5.kdenlive
-                mkvtoolnix
-                mediainfo-gui
-                qmplay2
-                ffmpeg-normalize
-
-                # Office
-                libreoffice-qt
-                
-                # CLI
-                appimage-run
-                bastet
-                bottom
-                broot
-                compsize
-                psmisc
-                lm_sensors
-                ffmpeg_6-full
-                fastfetch
-                python39Packages.secretstorage
-                s-tui
-                xsensors
-                ventoy-bin
-                bluetooth_battery
-                scrcpy
-                                
-                # System apps
-                libsForQt5.ark
-                far2l
-                krusader
-                psensor
-                qdirstat
-                qrcp
-                testdisk-qt
-                lxqt.libfm-qt
-                xclip
-                xournalpp
-                yarn
-                
-                # System components
-                papirus-icon-theme
-                polkit
-                plasma-hud
-                libsForQt5.bismuth
-                rar
-                unixtools.quota
-                polkit
-                papirus-maia-icon-theme
-                luna-icons
-                material-icons
-                libplacebo
-                python3Full
-                glxinfo
-                vulkan-tools
-                wayland-utils
-                xorg.xdpyinfo
-                xorg.xinit
-                ];
-                stateVersion = "23.05";
+  services = {
+    syncthing.enable = true;
+	  easyeffects.enable = true;
+    };
+    programs = {
+    btop.enable = true;
+    bash.enable = true;
+    yt-dlp.enable = true;
+    alacritty.enable = true;
+    wezterm.enable = true;
+    git.enable = true;
+    java.enable = true;
+    zsh.enable = true;
+    mpv.enable = true;
+    nnn.enable = true;
+    aria2 = {
+      enable = false;
+      settings =  {
+        enable-rpc = true;
+        rpc-listen-all = true;
+        rpc-allow-origin-all = true;
+        rpc-secret = "september";
+        disk-cache = "32M"; 
+        force-save = true;
+        save-session = "$HOME/.config/aria2/aria2.session";
+        auto-save-interval = 10;
+        save-session-interval = 10;
+        allow-overwrite = true; 
+        file-allocation = "none";
+        bt-enable-lpd = true;
+        conditional-get = true; 
+        check-integrity = true;
+        max-concurrent-downloads = 10; 
+        max-connection-per-server = 3; 
+        seed-ratio = 0;
+        enable-dht6 = true;
+        bt-max-peers = 255;
+        follow-torrent = "mem";
+        min-split-size = "1M";
+        split = 10;
         };
-        services = {
-					syncthing.enable = true;
-					easyeffects.enable = true;
-        };
-        programs = {
-					btop.enable = true;
-					bash.enable = true;
-					yt-dlp.enable = true;
-	        alacritty.enable = true;
-	        wezterm.enable = true;
-	        git.enable = true;
-	        java.enable = true;
-	        zsh.enable = true;
-	        mpv.enable = true;
-	        nnn.enable = true;
-					aria2 = {
-						enable = false;
-						settings =  {
-							enable-rpc = true;
-							rpc-listen-all = true;
-							rpc-allow-origin-all = true;
-							rpc-secret = "september";
-							disk-cache = "32M"; 
-							force-save = true;
-							save-session = "$HOME/.config/aria2/aria2.session";
-							auto-save-interval = 10;
-							save-session-interval = 10;
-							allow-overwrite = true; 
-							file-allocation = "none";
-							bt-enable-lpd = true;
-							conditional-get = true; 
-							check-integrity = true;
-							max-concurrent-downloads = 10; 
-							max-connection-per-server = 3; 
-							seed-ratio = 0;
-							enable-dht6 = true;
-							bt-max-peers = 255;
-							follow-torrent = "mem";
-							min-split-size = "1M";
-							split = 10;
-							};
-					};
-        };
+    };
+  };
 };
         
 # List packages installed in system profile. To search, run:
