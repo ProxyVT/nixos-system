75185fdfaf04b6bd53b46f9cc0a0e92451a87531
ProxyVT <tikit.us@outlook.com>
2023-08-15 10:24:54 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,30 +1,30 @@
 {
-  description = "My first flake config";
+  description = "My fist flake config";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
+  	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  	home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-    in
-    rec {
-      nixosConfigurations = {
-        ulad = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [
-      	    ./nixos/configuration.nix
-      	  ];
-      	};
-      };	
+  
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      ulad = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+					./nixos/configuration.nix
+					./applications/environment.nix
+					 home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ulad = import ./applications/dotfiles.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+				];
+			};
     };
+  };
 }
