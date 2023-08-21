f42e88fe95e81a7ab9655e526cb696eec0ec574f
Ulad <tikit.us@outlook.com>
2023-08-21 10:42:30 +0300

    Update gtx.nix

--- hardware/gtx.nix
+++ hardware/gtx.nix
@@ -1,5 +1,9 @@
 { inputs, outputs, lib, config, pkgs, ... }:
 
 {
-  
+  services.xserver.videoDrivers = [ "nvidiaLegacy470" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+  };
 }
