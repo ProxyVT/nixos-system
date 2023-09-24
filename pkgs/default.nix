4a1247a3ccc1a07892020919ec4a2f895b7f41ad
ProxyVT <tikit.us@outlook.com>
2023-09-24 19:03:34 +0300

    Update default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,5 @@
-{ inputs, outputs, lib, config, pkgs, ... }:
-# Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example'
-pkgs: {
-  mpv-git = pkgs.callPackage ./mpv-git.nix { };
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [ (pkgs.callPackage ./mpv-git.nix {}) ];
 }
