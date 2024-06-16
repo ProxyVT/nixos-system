a49cc053b1f41e34845176b173a7e7ed4d6691d9
ProxyVT <tikit.us@outlook.com>
2024-06-16 19:33:44 +0300

    Update environment.nix

--- applications/environment.nix
+++ applications/environment.nix
@@ -1,53 +1,12 @@
 { inputs, outputs, lib, config, pkgs, ... }:
 
-{ 
-  nixpkgs = {
-    overlays = [
-      (final: prev: {
-        uosc-git = prev.mpvScripts.uosc.overrideAttrs (oldAttrs: rec {
-          pname = "uosc";
-          version = "git";
-          src = prev.fetchgit {
-            url = "https://github.com/tomasklaen/uosc.git";
-            rev = "6fa34c31d0a5290dee83282205768d15111df7d8";
-            hash = "sha256-qxyNZHmH33bKRp4heFSC+RtvSApIfbVFt4otfS351nE=";
-          };
-          patches = [];
-        });
-        mpv-unwrapped = (prev.mpv-unwrapped.override {
-          libplacebo = prev.libplacebo.overrideAttrs (oldAttrs: rec {
-            pname = "libplacebo";
-            version = "git";
-            src = prev.fetchgit {
-              url = "https://code.videolan.org/videolan/libplacebo.git";
-              rev = "311a59507f6a0465aaac9b783af65bf349755360";
-              hash = "sha256-rwaufc4LfcX190ulHv0NPuER/D7//SwoLrSN4kKteqk=";
-            };
-            buildInputs = oldAttrs.buildInputs ++ [ pkgs.xxHash ];
-          });  
-        }).overrideAttrs ( oldAttrs: rec {
-          pname = "mpv";
-          version = "git";
-          src = prev.fetchgit {
-            url = "https://github.com/mpv-player/mpv.git";
-            rev = "2af3a6e294e829191dfa0c41396ecd6384d405d9";
-            hash = "sha256-y4v4I88a9KRgrvuWJBGM8Q3lG1WUXrUW0O7qTCrk9nk=";
-          };
-          patches = [];
-        });
-        mpv-git = pkgs.wrapMpv final.mpv-unwrapped {
-          scripts = with pkgs; [ 
-            uosc-git
-            mpvScripts.thumbfast
-          ];
-        };      
-      })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      allowUnfree = true;
-    };
-  };
+{
+  imports = 
+  [
+    ./overlays/mpv.nix
+    ./overlays/uosc.nix
+    ./overlays/xz.nix
+  ];
   
   fonts.packages = with pkgs; [
     liberation_ttf
@@ -62,11 +21,12 @@
   
     # Development
     super-productivity
-    android-studio
-    eclipses.eclipse-java
     github-desktop
     nodejs
+    cudatext
+    lite-xl
     nodePackages.gulp
+    nodePackages.pnpm
     wineWowPackages.unstableFull
     
     # Graphics
@@ -75,39 +35,28 @@
     pinta
     blanket
     libsForQt5.spectacle
-    nomacs
     digikam
     
     # Internet
-    vivaldi
-    vivaldi-ffmpeg-codecs
     you-get
     browsh
-    cloudflare-warp
     firefox
     tdesktop
     motrix
-    streamlink
     ariang
     media-downloader
-    tartube-yt-dlp
     
     # Server & security
     keepassxc
-    john
-    putty
-    protonvpn-gui
     ddrescue
     ddrescueview
+    electrum
     
     # Multimedia
-    audacity
     handbrake
-    libsForQt5.kdenlive
     mkvtoolnix
     mediainfo-gui
     qmplay2
-    ffmpeg-normalize
     mpv-git
     mousai
     
@@ -126,7 +75,7 @@
     ffmpeg
     fastfetch
     tuifimanager
-    python39Packages.secretstorage
+    #python39Packages.secretstorage
     s-tui
     xsensors
     ventoy-bin
@@ -136,10 +85,12 @@
     ttop
     ddcutil
     brightnessctl
+    nurl
+    nix-tree
+    fio
+    ffmpeg-normalize
     
     # System apps
-    libsForQt5.ark
-    libsForQt5.dolphin
     psensor
     qdirstat
     qrcp
@@ -152,11 +103,14 @@
     darkman
     localsend
     grsync
+    nix-prefetch-git
+    nix-prefetch-scripts
     
     # System components
     papirus-icon-theme
     polkit
     plasma-hud
+    exfat
     rar
     unixtools.quota
     polkit
@@ -170,11 +124,15 @@
     wayland-utils
     xorg.xdpyinfo
     xorg.xinit
+    libimobiledevice
+    ifuse
   ];
 
   services = {
-    mullvad-vpn.enable = true; 	            # Mullvad VPN support
-    flatpak.enable = false; 		            # Flatpak support
+    mullvad-vpn = {
+      enable = true; 	                      # Mullvad VPN support
+      package = pkgs.mullvad-vpn;
+    };
     cinnamon.apps.enable = true;            # Cinnamon apps
     gnome.gnome-keyring.enable = true;      # Gnome keyring support
     transmission = {                        # Transmission torrent service
@@ -189,6 +147,10 @@
       enableWebUI = true;
       httpListenPort = 8888;
       httpListenAddr = "0.0.0.0";
+    };
+    usbmuxd = {
+      enable = true;
+      package = pkgs.usbmuxd2;
     };
   };
 
@@ -214,23 +176,15 @@
     };
     steam = {
       enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
+      gamescopeSession.enable = true;
     };
     git = {
       enable = true;
       package = pkgs.gitFull;
     };
-    file-roller.enable = true;
     gamemode.enable = true;
-    thunar = {
-      plugins = with pkgs.xfce; [
-        thunar-archive-plugin
-        thunar-volman
-        thunar-media-tags-plugin
-      ];
-    };
     gnome-disks.enable = true;
+    partition-manager.enable = true;
     mtr.enable = true;
     gnupg.agent = {
       enable = true;
@@ -238,5 +192,3 @@
     };
   };
 }
-
-
