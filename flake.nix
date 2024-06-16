a53e413d19ca78e3a01975454988441e979d98fa
ProxyVT <tikit.us@outlook.com>
2024-06-16 19:12:28 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "Personal flake configuration";
   
   inputs = {
-    #  Master branch
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    nixpkgs-staging-next.url = "github:nixos/nixpkgs/staging-next";
-  	home-manager.url = "github:nix-community/home-manager";
+    master.url = "github:nixos/nixpkgs/master";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   
@@ -18,23 +17,18 @@
     inherit (self) outputs;
       # Supported systems for your flake packages, shell, etc.
       systems = [
-        "aarch64-linux"
-        "i686-linux"
         "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
       # This is a function that generates an attribute by calling a function you
       # pass to it, with each system as an argument
       forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     nixosConfigurations = {
-      ulad-acer = nixpkgs.lib.nixosSystem {
+      ulad = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs outputs; };
         modules = [
 	        ./nixos/configuration.nix
 	        ./applications/environment.nix
-          ./hardware/acer.nix
 	       home-manager.nixosModules.home-manager
           {
             home-manager = {
@@ -43,39 +37,7 @@
               users.ulad = import ./applications/dotfiles.nix;
             };
           }
-	      ];
-      };
-      ulad-gtx = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs outputs; };
-        modules = [
-          ./nixos/configuration.nix
-          ./applications/environment.nix
-          ./hardware/gtx.nix
-	      home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.ulad = import ./applications/dotfiles.nix;
-            };
-          }
-	      ];
-      };
-      ulad-rtx = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs outputs; };
-        modules = [
-          ./nixos/configuration.nix
-          ./applications/environment.nix
-          ./hardware/rtx.nix
-	      home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.ulad = import ./applications/dotfiles.nix;
-            };
-          }
-        ];
+	];
       };
     };
   };
