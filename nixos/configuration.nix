f678c68a5a05f5cf459df2798c4813f415be5eb4
ProxyVT <tikit.us@outlook.com>
2024-06-25 20:55:40 +0300

    Fix persistent path and delete home persistent config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -194,7 +194,7 @@
     };
   };      
 
-  environment.persistence."/nix/persistent" = {
+  environment.persistence."/persistent" = {
     hideMounts = true;
     directories = [
       "/var/log"
@@ -208,21 +208,6 @@
       "/etc/machine-id"
       { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
-    users.ulad = {
-      directories = [
-        "Downloads"
-        "Music"
-        "Pictures"
-        "Documents"
-        "Videos"
-        "VirtualBox VMs"
-        { directory = ".gnupg"; mode = "0700"; }
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".nixops"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
-        ".local/share/direnv"
-      ];
-    };
   };
 
   system = {
