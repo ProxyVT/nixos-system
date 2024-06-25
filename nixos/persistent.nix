f9317a688986edc8ac0a3d6df86dfa26e9584352
ProxyVT <tikit.us@outlook.com>
2024-06-25 21:23:26 +0300

    Update persistent.nix

--- nixos/persistent.nix
+++ nixos/persistent.nix
@@ -15,4 +15,27 @@
       { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
   };
+  home.persistence."/persistent/home/ulad" = {
+    directories = [
+      "Downloads"
+      "Music"
+      "Pictures"
+      "Documents"
+      "Videos"
+      "VirtualBox VMs"
+      ".gnupg"
+      ".ssh"
+      ".nixops"
+      ".local/share/keyrings"
+      ".local/share/direnv"
+      {
+        directory = ".local/share/Steam";
+        method = "symlink";
+      }
+    ];
+    files = [
+      ".screenrc"
+    ];
+    allowOther = true;
+  };
 }
