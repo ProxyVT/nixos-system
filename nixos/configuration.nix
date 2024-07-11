71429fb8897c4ec30f08c91e95960d64f4babff1
ProxyVT <tikit.us@outlook.com>
2024-07-11 20:56:48 +0300

    Back to latest nixos version

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -29,7 +29,7 @@
   
   # Nix configuration
   nix = {
-    #package = pkgs.nixVersions.latest;
+    package = pkgs.nixVersions.latest;
     channel.enable = false;
     settings = {
       auto-optimise-store = true;                       # Store optimization	
