ce21f4fce3f8f203aa23419c12a766dade69fc3b
ProxyVT <tikit.us@outlook.com>
2024-07-31 11:13:51 +0300

    Add hardware configs for laptop and desktop

--- hardware/acer.nix
+++ hardware/acer.nix
@@ -1,1 +1,13 @@
+{ config, lib, pkgs, modulesPath, ... }:
 
+services.xserver.videoDrivers = [ "nvidia" ];
+
+hardware.nvidia = {
+  modesetting.enable = true;
+  nvidiaSettings = true;
+  prime = {
+    reverseSync.enable = true;
+    nvidiaBusId = "PCI:4:0:0";
+    intelBusId = "PCI:0:2:0";
+  };  
+};
--- hardware/umka.nix
+++ hardware/umka.nix
@@ -1,1 +1,8 @@
+{ config, lib, pkgs, modulesPath, ... }:
 
+services.xserver.videoDrivers = [ "nvidia" ];
+
+hardware.nvidia = {
+  modesetting.enable = true;
+  package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+};
