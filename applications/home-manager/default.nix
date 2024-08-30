ca9c29e10fec0021be59bf07e0ad9a612bb3b1a5
ProxyVT <tikit.us@outlook.com>
2024-08-30 12:12:19 +0300

    Update default.nix

--- applications/home-manager/default.nix
+++ applications/home-manager/default.nix
@@ -1,1 +1,38 @@
+{ inputs, outputs, lib, config, pkgs, ... }: {
 
+  imports = [
+    ./aria2.nix
+    ./bash.nix
+    ./btop.nix
+    ./firefox.nix
+    ./mangohud.nix
+    ./thunderbird.nix
+    ./wezterm.nix
+    ./yt-dlp.nix
+  ];
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);     # Workaround for https://github.com/nix-community/home-manager/issues/2942
+    };
+  };
+
+  home = {
+    username = "ulad";
+  };
+
+  services = {
+    easyeffects.enable = false;
+    gpg-agent.enable = true;
+  };
+
+  programs = {
+    home-manager.enable = true;
+    java.enable = true;
+    chromium.enable = true;
+    gpg.enable = true;
+  };
+
+  home.stateVersion = "24.05";
+}
