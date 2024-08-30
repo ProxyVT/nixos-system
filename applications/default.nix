ad047b30b8cc88ace963a0b40dce8ae51a0d1c44
ProxyVT <tikit.us@outlook.com>
2024-08-30 12:15:12 +0300

    Update default.nix

--- applications/default.nix
+++ applications/default.nix
@@ -1,50 +1,7 @@
-{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; }
-, lib ? pkgs.lib
-, fetchFromGitHub ? pkgs.fetchFromGitHub
-, makeFontsConf ? pkgs.makeFontsConf
-, buildLua ? pkgs.buildLua
-, buildGoModule ? pkgs.buildGoModule
-}:
+{ inputs, outputs, lib, config, pkgs, ... }: {
 
-buildLua (finalAttrs: {
-  pname = "uosc";
-  version = "5.1.1";
-  scriptPath = "src/uosc";
-
-  src = fetchFromGitHub {
-    owner = "tomasklaen";
-    repo = "uosc";
-    rev = finalAttrs.version;
-    hash = "sha256-+4k8T1yX3IRXK3XkUShsuJSH9w1Zla7CaRENcIqX4iM=";
-  };
-
-  tools = buildGoModule {
-    pname = "uosc-bin";
-    inherit (finalAttrs) version src;
-    vendorHash = "sha256-nkY0z2GiDxfNs98dpe+wZNI3dAXcuHaD/nHiZ2XnZ1Y=";
-  };
-
-  # Patch lua script to get the path to its `ziggy` binary form the environment
-  patches = [ ./uosc.patch ];
-
-  # the script uses custom "texture" fonts as the background for ui elements.
-  # In order for mpv to find them, we need to adjust the fontconfig search path.
-  postInstall = "cp -r src/fonts $out/share";
-  passthru.extraWrapperArgs = [
-    "--set"
-    "FONTCONFIG_FILE"
-    (toString (makeFontsConf {
-      fontDirectories = [ "${finalAttrs.finalPackage}/share/fonts" ];
-    }))
-    "--set"
-    "ZIGGY"
-    (lib.getExe' finalAttrs.tools "ziggy")
+  imports = [
+    ./home-manager/default.nix
   ];
 
-  meta = with lib; {
-    description = "Feature-rich minimalist proximity-based UI for MPV player";
-    homepage = "https://github.com/tomasklaen/uosc";
-    license = licenses.gpl3Only;
-    maintainers = with lib.maintainers; [ apfelkuchen6 ];
-  };
-})
+}
