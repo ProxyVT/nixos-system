b234ba96a536beeb5d05c5820d2e16898d9b0237
ProxyVT <tikit.us@outlook.com>
2024-09-28 20:05:33 +0300

    Back to "classic"
    For less compilation from source.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Personal flake configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
     nixpkgs-local.url = "path:/home/ulad/Documents/GitHub/nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
