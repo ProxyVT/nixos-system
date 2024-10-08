6c087befb56f469eb374b48eaef8ccb9ced287b9
ProxyVT <tikit.us@outlook.com>
2024-10-08 11:29:23 +0300

    Create icon_fix.nix

---
+++ applications/home-manager/icon_fix.nix
@@ -0,0 +1,15 @@
+{ lib, config, pkgs, ... }: {
+
+home.activation.copyDesktopFiles = lib.hm.dag.entryAfter ["installPackages"] ''
+    if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
+      if [ -d "${config.home.homeDirectory}/.nix-profile/share/applications" ]; then
+        rm -rf ${config.home.homeDirectory}/.local/share/applications
+        mkdir -p ${config.home.homeDirectory}/.local/share/applications
+        for file in ${config.home.homeDirectory}/.nix-profile/share/applications/*; do
+          ln -sf "$file" ${config.home.homeDirectory}/.local/share/applications/
+        done
+      fi
+    fi
+  '';
+
+}
