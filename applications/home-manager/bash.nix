fb9fa871e50da5cd41ffbc27b1cedaa1088f8771
ProxyVT <tikit.us@outlook.com>
2024-11-07 04:27:10 +0300

    Update bash.nix
    - Removed unused variables
    - Added aliases for some nix options

--- applications/home-manager/bash.nix
+++ applications/home-manager/bash.nix
@@ -1,10 +1,14 @@
-{ lib, config, pkgs, ... }: {
+{ ... }: {
 
   programs.bash = {
     enable = true;
-    bashrcExtra = "
-      alias sudo=doas
-    ";
+    shellAliases = {
+      sudo = "doas";
+      nix-gc = "doas nix-collect-garbage -d ; nix-collect-garbage -d";
+      boot = "doas nixos-rebuild boot --flake";
+      switch = "doas nixos-rebuild switch --flake";
+      build = "nixos-rebuild build --flake";
+    };
   };
 
 }
