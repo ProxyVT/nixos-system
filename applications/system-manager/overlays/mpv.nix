13d5cb1d4308b2fc4b334096b9ccda341c91a950
ProxyVT <tikit.us@outlook.com>
2024-11-17 21:10:14 +0300

    Update mpv.nix

--- applications/system-manager/overlays/mpv.nix
+++ applications/system-manager/overlays/mpv.nix
@@ -13,8 +13,8 @@
             version = "git";
             src = prev.fetchgit {
               url = "https://github.com/haasn/libplacebo.git";
-              rev = "a018ab049edd12e4b36ce9649eb785cccc744705";
-              hash = "sha256-C6LwgYvIJLG98uZpyUaWg/AwUZblORam+Er1DEYp/y0=";
+              rev = "5788a82f459f617a999c4d56278d54d0edfc7b81";
+              hash = "sha256-d7PoHcfUYS7HYxW4CJ9b0bxIdUuP6GF19/8nCYFMgxM=";
             };
           });
           ffmpeg = prev.ffmpeg_7-full;
@@ -23,10 +23,24 @@
           version = "git";
           src = prev.fetchgit {
             url = "https://github.com/mpv-player/mpv.git";
-            rev = "1c55d600ab7df360acf1403d9586892600371166";
-            hash = "sha256-a+UtBLV5+sQQKtCVMhLywLv4vmxaH4IZh/pHx93O898=";
+            rev = "70aaba71d6e3071a732069a1d222d1eb4293faf2";
+            hash = "sha256-sRfmQdwt3l2O0Pik2K+4LTjLbxrAMZ7CvpAnk6zAkrI=";
           };
           patches = [];
+          postPatch = lib.concatStringsSep "\n" [
+            ''
+            substituteInPlace meson.build \
+              --replace-fail "conf_data.set_quoted('CONFIGURATION', meson.build_options())" \
+                            "conf_data.set_quoted('CONFIGURATION', '<ommited>')"
+            ''
+            ''
+            pushd TOOLS
+            mv mpv_identify.sh mpv_identify
+            patchShebangs *.py *.sh
+            mv mpv_identify mpv_identify.sh
+            popd
+            ''
+          ];
         });
         mpv-git = final.mpv.override {
           scripts = with pkgs; [
