6133b90d54c1f82ef1b087f3652ba84e3478d7c1
ProxyVT <tikit.us@outlook.com>
2024-12-22 23:35:45 +0300

    Update steam.nix

--- applications/system-manager/steam.nix
+++ applications/system-manager/steam.nix
@@ -2,7 +2,9 @@
 
 programs.steam = {
   enable = true;
-  gamescopeSession.enable = true;
+  remotePlay.openFirewall = true;
+  dedicatedServer.openFirewall = true;
+  localNetworkGameTransfers.openFirewall = true; 
 };
 
 }
