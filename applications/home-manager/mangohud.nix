9d1fceba56c68b4eb3c336213153c46d3476b00b
ProxyVT <tikit.us@outlook.com>
2025-01-16 16:22:37 +0300

    Update mangohud.nix
    Expand mangohud settings for mpv to all packages

--- applications/home-manager/mangohud.nix
+++ applications/home-manager/mangohud.nix
@@ -3,14 +3,9 @@
   programs.mangohud = {
     enable = true;
     enableSessionWide = true;
-    settingsPerApplication = {
-      mpv = {
-        full = true;
-        no_display = true;
-      };
-    };
     settings = {
       full = true;
+      no_display = true;
     };
   };
 
