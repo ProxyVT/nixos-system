c21fb6a69bedc405268700f43372486fd8582440
ProxyVT <tikit.us@outlook.com>
2025-01-21 22:56:49 +0300

    Update default.nix
    - Fixed tabs and other stuff
    - Added various packages + cross compiled DXVK
    - Removed usbmuxd from imports and added to default services

--- applications/system-manager/default.nix
+++ applications/system-manager/default.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
 
   imports = [
     ./overlays/mpv.nix
@@ -9,7 +10,6 @@
     ./ssh.nix
     ./steam.nix
     ./transmission.nix
-    ./usbmuxd.nix
   ];
 
   fonts.packages = with pkgs; [
@@ -21,144 +21,153 @@
     inter
   ];
 
-  environment.systemPackages = with pkgs; [
+  environment = {
+    systemPackages = with pkgs; [
 
-    # Development
-    super-productivity
-    github-desktop
-    nodejs
-    cudatext-gtk
-    lapce
-    lite-xl
-    nodePackages.gulp
-    nodePackages.pnpm
-    wineWowPackages.unstableFull
-    nixpkgs-review
-    zed-editor
-    master.warp-terminal
-    master.waveterm
-    master.windterm
+      # Development
+      super-productivity
+      github-desktop
+      nodejs
+      cudatext-gtk
+      lite-xl
+      nodePackages.gulp
+      nodePackages.pnpm
+      wineWowPackages.unstableFull
+      nixpkgs-review
+      zed-editor
+      warp-terminal
+      waveterm
+      windterm
+      ghostty
 
-    # Graphics
-    gpick
-    krita
-    pinta
-    blanket
-    libsForQt5.spectacle
-    redshift
+      # Graphics
+      edge.gpick
+      krita
+      pinta
+      blanket
+      libsForQt5.spectacle
 
-    # Internet
-    you-get
-    browsh
-    vivaldi
-    motrix
-    master.persepolis
-    ariang
-    media-downloader
-    element-desktop
-    telegram-desktop
+      # Internet
+      you-get
+      browsh
+      vivaldi
+      motrix
+      edge.persepolis
+      ariang
+      media-downloader
+      element-desktop
+      telegram-desktop
 
-    # Server & security
-    keepassxc
-    ddrescue
-    ddrescueview
-    release.electrum
+      # Server & security
+      keepassxc
+      ddrescue
+      ddrescueview
+      electrum
+      tradingview
 
-    # Multimedia
-    audacity
-    handbrake
-    mkvtoolnix
-    mediainfo-gui
-    qmplay2
-    mpv-git
-    mousai
-    subtitleedit
+      # Multimedia
+      audacity
+      handbrake
+      mkvtoolnix
+      mediainfo-gui
+      qmplay2
+      mpv-git
+      mousai
+      subtitleedit
 
-    # Games
-    pcsx2
-    heroic
-    libstrangle
+      # Games
+      pcsx2
+      libstrangle
 
-    # Office
-    libreoffice-qt
-    simple-scan
+      # Office
+      libreoffice-qt
+      simple-scan
 
-    # CLI
-    appimage-run
-    vrrtest
-    bastet
-    bottom
-    broot
-    psmisc
-    lm_sensors
-    ffmpeg
-    fastfetch
-    tuifimanager
-    python312Packages.secretstorage
-    s-tui
-    xsensors
-    ventoy-full
-    bluetooth_battery
-    scrcpy
-    speedtest-cli
-    ttop
-    ddcutil
-    brightnessctl
-    nurl
-    nix-tree
-    fio
-    ffmpeg-normalize
-    toybox
+      # CLI
+      appimage-run
+      vrrtest
+      bastet
+      bottom
+      broot
+      psmisc
+      lm_sensors
+      ffmpeg-full
+      fastfetch
+      tuifimanager
+      python312Packages.secretstorage
+      s-tui
+      xsensors
+      ventoy-full
+      bluetooth_battery
+      scrcpy
+      speedtest-cli
+      ttop
+      ddcutil
+      brightnessctl
+      nurl
+      nix-tree
+      fio
+      ffmpeg-normalize
+      toybox
+      nixfmt-rfc-style
 
-    # System apps
-    release.psensor
-    qdirstat
-    qrcp
-    testdisk-qt
-    lxqt.libfm-qt
-    xclip
-    xournalpp
-    yarn
-    pavucontrol
-    darkman
-    grsync
-    nix-prefetch-git
-    nix-prefetch-scripts
-    mission-center
+      # System apps
+      resources
+      monitorets
+      qdirstat
+      qrcp
+      testdisk-qt
+      xclip
+      xournalpp
+      yarn
+      pavucontrol
+      darkman
+      grsync
+      edge.rsync
+      nix-prefetch-git
+      nix-prefetch-scripts
+      mission-center
+      nemo-with-extensions
+      edge.peazip
+      picom
 
-    # System components
-    nixd
-    papirus-icon-theme
-    polkit
-    plasma-hud
-    exfat
-    rar
-    unixtools.quota
-    polkit
-    papirus-maia-icon-theme
-    release.luna-icons
-    libva-utils
-    material-icons
-    python3Full
-    glxinfo
-    vulkan-tools
-    wayland-utils
-    xorg.xdpyinfo
-    xorg.xinit
-    libimobiledevice
-    ifuse
-    gst_all_1.gst-libav
-    gst_all_1.gst-plugins-ugly
-    gcc
-    cmake
-    xfce.tumbler
-    xfce.xfce4-xkb-plugin
-  ];
+      # System components
+      nixd
+      papirus-icon-theme
+      polkit
+      plasma-hud
+      exfat
+      rar
+      unixtools.quota
+      polkit
+      papirus-maia-icon-theme
+      legacy.luna-icons
+      libva-utils
+      material-icons
+      python3Full
+      glxinfo
+      vulkan-tools
+      wayland-utils
+      xorg.xdpyinfo
+      xorg.xinit
+      libimobiledevice
+      ifuse
+      gst_all_1.gst-libav
+      gst_all_1.gst-plugins-ugly
+      gcc
+      cmake
+    ] 
+    ++ 
+    (with pkgsCross; [
+      mingw32.dxvk_1
+      mingwW64.dxvk_1
+    ]);  
+  };  
 
   services = {
     cinnamon.apps.enable = false;           # Cinnamon apps
     gnome.gnome-keyring.enable = true;      # Gnome keyring support
-    usbmuxd.enable = true;
+    usbmuxd.enable = true;                  # Usbmuxd support
   };
 
   programs = {
