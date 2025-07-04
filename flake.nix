b977edf608d6806276afba0026809610ec69cb83
ProxyVT <tikit.us@outlook.com>
2025-07-05 01:37:01 +0300

    Nixfmt style

--- flake.nix
+++ flake.nix
@@ -16,67 +16,67 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-legacy,
-    nixpkgs-release,
-    nixpkgs-edge,
-    nixpkgs-testing,
-    nixpkgs-betterbird,
-    nixpkgs-skype,
-    impermanence,
-    home-manager,
-    ...
-  } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-legacy,
+      nixpkgs-release,
+      nixpkgs-edge,
+      nixpkgs-testing,
+      nixpkgs-betterbird,
+      nixpkgs-skype,
+      impermanence,
+      home-manager,
+      ...
+    }@inputs:
 
-  let
-    inherit (self) outputs;
-    system = [ "x86_64-linux" ];
-    forAllSystems = nixpkgs.lib.genAttrs system;
-  in {
-    overlays = import ./applications/system-manager/overlays { inherit inputs; };
-    nixosConfigurations = {
-      acer = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs outputs; };
-        modules = [
-          ./nixos
-          ./applications/system-manager
-          ./hardware/acer.nix
-          home-manager.nixosModules.home-manager
-          impermanence.nixosModules.impermanence
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.ulad = import ./applications/home-manager;
-            };
-          }
-        ];
-      };
-      umka = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs outputs; };
-        modules = [
-          ./nixos
-          ./applications/system-manager
-          ./hardware/umka.nix
-          home-manager.nixosModules.home-manager
-          impermanence.nixosModules.impermanence
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.ulad = import ./applications/home-manager;
-            };
-          }
-        ];
-      };
-      exampleIso = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs outputs; };
-        modules = [
-          ./iso/configuration.nix
-        ];
+    let
+      inherit (self) outputs;
+    in
+    {
+      overlays = import ./applications/system-manager/overlays { inherit inputs; };
+      nixosConfigurations = {
+        acer = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./nixos
+            ./applications/system-manager
+            ./hardware/acer.nix
+            home-manager.nixosModules.home-manager
+            impermanence.nixosModules.impermanence
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.ulad = import ./applications/home-manager;
+              };
+            }
+          ];
+        };
+        umka = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./nixos
+            ./applications/system-manager
+            ./hardware/umka.nix
+            home-manager.nixosModules.home-manager
+            impermanence.nixosModules.impermanence
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.ulad = import ./applications/home-manager;
+              };
+            }
+          ];
+        };
+        exampleIso = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./iso/configuration.nix
+          ];
+        };
       };
     };
-  };
 }
