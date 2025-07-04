990e869d403b80c043708afd7aaa297ed12375b2
ProxyVT <tikit.us@outlook.com>
2025-07-05 01:48:03 +0300

    Nixfmt style

--- applications/home-manager/modules/yt-dlp.nix
+++ applications/home-manager/modules/yt-dlp.nix
@@ -1,12 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
 
-programs.yt-dlp = {
-  enable = true;
-  package = pkgs.edge.yt-dlp;
-  settings = {
-    downloader = "aria2c";
-    merge-output-format = "mkv";
+  programs.yt-dlp = {
+    enable = true;
+    package = pkgs.edge.yt-dlp;
+    settings = {
+      downloader = "aria2c";
+      merge-output-format = "mkv";
+    };
   };
-};
 
 }
