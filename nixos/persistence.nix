1a3cbdb35038903d879396ff446b41821c3e1b69
ProxyVT <tikit.us@outlook.com>
2025-07-06 11:39:38 +0300

    Nixfmt style

--- nixos/persistence.nix
+++ nixos/persistence.nix
@@ -1,48 +1,64 @@
-{ ... }: {
+{ ... }:
+{
 
-environment.persistence."/persist" = {
-  hideMounts = true;
-  
-  directories = [
-    "/var/log"
-    "/var/lib/bluetooth"
-    "/var/lib/nixos"
-    "/var/lib/systemd/coredump"
-    "/etc/NetworkManager/system-connections"
-    "/etc/mullvad-vpn"
-    { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
-  ];
+  environment.persistence."/persist" = {
+    hideMounts = true;
 
-  files = [
-    { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
-  ];
-
-  users.ulad = {
     directories = [
-      "Downloads"
-      "Music"
-      "Pictures"
-      "Documents"
-      "Videos"
-      "VirtualBox VMs"
-      "nixos-system"
-      ".gnupg"
-      ".ssh"
-      ".nixops"
-      ".config"
-      ".local"
-      ".mozilla"
-      ".thunderbird"
-      ".cache/thumbnails"
-      ".cache/nix"
-      ".cache/fontconfig"
-      ".wine"
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+      "/var/lib/transmission"
+      "/var/lib/resilio-sync"
+      "/var/lib/iwd"
+      "/etc/NetworkManager/system-connections"
+      "/etc/mullvad-vpn"
+      {
+        directory = "/var/lib/colord";
+        user = "colord";
+        group = "colord";
+        mode = "u=rwx,g=rx,o=";
+      }
     ];
+
     files = [
-      ".screenrc"
-      ".bash_history"
+      "/var/lib/swapfile"
+      {
+        file = "/var/keys/secret_file";
+        parentDirectory = {
+          mode = "u=rwx,g=,o=";
+        };
+      }
     ];
+
+    users.ulad = {
+      directories = [
+        "Downloads"
+        "Music"
+        "Pictures"
+        "Documents"
+        "Videos"
+        "VirtualBox VMs"
+        "Sync"
+        "nixos-system"
+        ".gnupg"
+        ".ssh"
+        ".nixops"
+        ".config"
+        ".local"
+        ".mozilla"
+        ".thunderbird"
+        ".cache/thumbnails"
+        ".cache/nix"
+        ".cache/fontconfig"
+        ".wine"
+      ];
+      files = [
+        ".screenrc"
+        ".bash_history"
+      ];
+    };
   };
-};
 
 }
