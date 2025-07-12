b5cad3f9e3ecf83b299832de2d7c20a193ff6545
ProxyVT <tikit.us@outlook.com>
2025-07-12 22:47:15 +0300

    Nixfmt style

--- applications/home-manager/modules/aria2.nix
+++ applications/home-manager/modules/aria2.nix
@@ -1,38 +1,37 @@
-{ pkgs, ... }: {
-
-programs.aria2 = {
-  enable = true;
-  settings = {
-    enable-rpc = true;
-    rpc-listen-all = false;
-    rpc-allow-origin-all = false;
-    rpc-secret = "september";
-    disk-cache = "256M";
-    force-save = true;
-    continue = true;
-    pause-metadata = true;
-    save-session = "/home/ulad/.config/aria2/aria2.session";
-    input-file = "/home/ulad/.config/aria2/aria2.session";
-    on-download-stop = "/home/ulad/.config/aria2/aria2.sh";
-    auto-save-interval = 10;
-    save-session-interval = 10;
-    allow-overwrite = true;
-    file-allocation = "none";
-    bt-enable-lpd = true;
-    bt-save-metadata = true;
-    bt-load-saved-metadata = true;
-    bt-remove-unselected-file = true;
-    bt-detach-seed-only = true;
-    bt-max-peers = 80;
-    conditional-get = true;
-    max-concurrent-downloads = 10;
-    enable-dht6 = false;
-    disable-ipv6 = true;
-    dht-listen-port = "50000-50100";
-    seed-ratio = 0;
-    follow-torrent = "mem";
-    min-split-size = "1M";
+{ ... }:
+{
+  programs.aria2 = {
+    enable = true;
+    settings = {
+      enable-rpc = true;
+      rpc-listen-all = false;
+      rpc-allow-origin-all = false;
+      rpc-secret = "september";
+      disk-cache = "256M";
+      force-save = true;
+      continue = true;
+      pause-metadata = true;
+      save-session = "/home/ulad/.config/aria2/aria2.session";
+      input-file = "/home/ulad/.config/aria2/aria2.session";
+      on-download-stop = "/home/ulad/.config/aria2/aria2.sh";
+      auto-save-interval = 10;
+      save-session-interval = 10;
+      allow-overwrite = true;
+      file-allocation = "none";
+      bt-enable-lpd = true;
+      bt-save-metadata = true;
+      bt-load-saved-metadata = true;
+      bt-remove-unselected-file = true;
+      bt-detach-seed-only = true;
+      bt-max-peers = 80;
+      conditional-get = true;
+      max-concurrent-downloads = 10;
+      enable-dht6 = false;
+      disable-ipv6 = true;
+      dht-listen-port = "50000-50100";
+      seed-ratio = 0;
+      follow-torrent = "mem";
+      min-split-size = "1M";
+    };
   };
-};
-
 }
