0a9b4c66d89983d1964b81bbaa69ff22c1de3697
ProxyVT <tikit.us@outlook.com>
2025-07-12 22:45:45 +0300

    Rewrite settings with DeepSeek advice

--- applications/home-manager/modules/aria2.nix
+++ applications/home-manager/modules/aria2.nix
@@ -1,18 +1,19 @@
-{ ... }: {
+{ pkgs, ... }: {
 
 programs.aria2 = {
   enable = true;
-  settings =  {
+  settings = {
     enable-rpc = true;
-    rpc-listen-all = true;
-    rpc-allow-origin-all = true;
+    rpc-listen-all = false;
+    rpc-allow-origin-all = false;
     rpc-secret = "september";
-    disk-cache = "64M";
+    disk-cache = "256M";
     force-save = true;
     continue = true;
     pause-metadata = true;
     save-session = "/home/ulad/.config/aria2/aria2.session";
     input-file = "/home/ulad/.config/aria2/aria2.session";
+    on-download-stop = "/home/ulad/.config/aria2/aria2.sh";
     auto-save-interval = 10;
     save-session-interval = 10;
     allow-overwrite = true;
@@ -22,11 +23,12 @@ programs.aria2 = {
     bt-load-saved-metadata = true;
     bt-remove-unselected-file = true;
     bt-detach-seed-only = true;
-    bt-max-peers = 128;
+    bt-max-peers = 80;
     conditional-get = true;
     max-concurrent-downloads = 10;
-    enable-dht6 = true;
-    dht-listen-port = "50101-50109";
+    enable-dht6 = false;
+    disable-ipv6 = true;
+    dht-listen-port = "50000-50100";
     seed-ratio = 0;
     follow-torrent = "mem";
     min-split-size = "1M";
