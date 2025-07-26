269f1380f727022defa856d09fde56f36183adc2
ProxyVT <tikit.us@outlook.com>
2025-07-26 09:33:58 +0300

    Switch to betterbird instead of thunderbird
    Temporally it will be using version 128.9, I will fix it later.

--- applications/home-manager/modules/thunderbird.nix
+++ applications/home-manager/modules/thunderbird.nix
@@ -1,11 +1,22 @@
-{ pkgs, ... }: {
+{ pkgs, thunderbirdPackages, ... }: {
 
 programs.thunderbird = {
   enable = true;
-  package = pkgs.thunderbird;
+  package = pkgs.bird.betterbird;
   profiles.ulad = {
     isDefault = true;
   };
+};
+
+nixpkgs = {
+  overlays = [
+    (final: prev: {
+      pkgs.bird.betterbird = prev.betterbird.overrideAttrs (oldAttrs: rec {
+        thunderbird-unwrapped = thunderbirdPackages.thunderbird-128;
+        version = "128.9.0esr";
+      });
+    })
+  ];
 };
 
 }
