3afe285956b8c63e9bcea2c97b3f48d47d6dd9dd
ProxyVT <tikit.us@outlook.com>
2025-07-31 11:55:11 +0300

    Update thunderbird.nix

--- applications/home-manager/modules/thunderbird.nix
+++ applications/home-manager/modules/thunderbird.nix
@@ -1,22 +1,23 @@
-{ pkgs, thunderbirdPackages, ... }: {
+{ pkgs, thunderbirdPackages, ... }:
+{
 
-programs.thunderbird = {
-  enable = true;
-  package = pkgs.bird.betterbird;
-  profiles.ulad = {
-    isDefault = true;
+  programs.thunderbird = {
+    enable = true;
+    package = pkgs.bird.betterbird;
+    profiles.ulad = {
+      isDefault = true;
+    };
   };
-};
 
-nixpkgs = {
-  overlays = [
-    (final: prev: {
-      pkgs.bird.betterbird = prev.betterbird.overrideAttrs (oldAttrs: rec {
-        thunderbird-unwrapped = thunderbirdPackages.thunderbird-128;
-        version = "128.9.0esr";
-      });
-    })
-  ];
-};
+  nixpkgs = {
+    overlays = [
+      (final: prev: {
+        pkgs.bird.betterbird = prev.betterbird.overrideAttrs (oldAttrs: {
+          thunderbird-unwrapped = thunderbirdPackages.thunderbird-128;
+          version = "128.9.0esr";
+        });
+      })
+    ];
+  };
 
 }
