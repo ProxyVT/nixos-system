ad33932b2c1be61629d5342313545f6f45adb4cb
ProxyVT <tikit.us@outlook.com>
2025-07-31 11:54:12 +0300

    Update resilio.nix

--- applications/system-manager/modules/resilio.nix
+++ applications/system-manager/modules/resilio.nix
@@ -5,6 +5,10 @@ services.resilio = {
   enableWebUI = true;
   httpListenPort = 8888;
   httpListenAddr = "0.0.0.0";
+  httpPass = "nixos";
+  httpLogin = "ulad";
+  #storagePath = "/home/ulad/.config/resilio";
+  directoryRoot = "/home/ulad/Sync";
 };
 
 }
