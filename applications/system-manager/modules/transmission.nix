8b5cabcccdc4480b208e4c8d0498625a56539d6f
ProxyVT <tikit.us@outlook.com>
2025-07-31 11:53:47 +0300

    Switch to transmission web version

--- applications/system-manager/modules/transmission.nix
+++ applications/system-manager/modules/transmission.nix
@@ -2,9 +2,12 @@
 
 services.transmission = {
   enable = true;
-  package = pkgs.transmission_4-gtk;
+  package = pkgs.transmission_4;
+  performanceNetParameters = true;
   settings = {
     umask = 0;
+    utp-enabled = true;
+    incomplete-dir-enabled = false;
   };
 };
 
