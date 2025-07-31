e5498bcc17f280c2533cb97ddc034e27b8065954
ProxyVT <tikit.us@outlook.com>
2025-07-31 12:03:12 +0300

    Massive refactoring
    - Added zswap
    - Changed zram and swappines params
    - Temporally disable Lix
    - Switched to iwd wi-fi backed
    - Added some printer options
    - Nixfmt style
    - Back to nixos-rebuild bash implementation
    - Etc. (some staff I may forgot and it eventually will be changed in the future).

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,143 +1,168 @@
-{ outputs, pkgs, lib, ... }: {
+{
+  outputs,
+  pkgs,
+  ...
+}:
 
-# System boot sections
-boot = {
-  kernelPackages = pkgs.linuxPackages_6_12;
-  kernel.sysctl = {
-    "net.ipv4.ip_default_ttl" = 65;       # Sync TTL to mobile
-    "vm.swappiness" = 1;
-  };
-  loader = {
-    systemd-boot = {
-      enable = true;                                  # Systemd-boot loader config
-      sortKey = "machine-id";                         # Sort specialisation generation
-    };
-    timeout = 10;                                     # Linux boot section timeout
-    efi.canTouchEfiVariables = true;
-  };
-};
-
-zramSwap = {                                          # Zram support
-  enable = true;
-  memoryPercent = 100;
-};
-
-# Nix configuration
-nix = {
-  package = pkgs.lix;
-  channel.enable = false;
-  distributedBuilds = true;
-  buildMachines = [{ 
-    hostName = "eu.nixbuild.net";
-    system = "x86_64-linux";
-    maxJobs = 100;
-    supportedFeatures = [ "benchmark" "big-parallel" ];
-  }];
-  settings = {
-    max-jobs = 8;
-    builders-use-substitutes = true;
-    substituters = lib.mkForce [ "https://nixos-cache-proxy.cofob.dev" ];
-    auto-optimise-store = true;                      
-    experimental-features = [                        
-      "nix-command"
-      "flakes"
-      "auto-allocate-uids"
+{
+  # System boot sections
+  boot = {
+    kernelPackages = pkgs.edge.linuxPackages_6_12;
+    kernelParams = [
+      "zswap.enabled=1"
+      "zswap.compressor=zstd"
+      "zswap.zpool=zsmalloc"
+      "zswap.max_pool_percent=25"
     ];
-  };
-};
-
-nixpkgs = {
-  config = {
-    allowUnfree = true;
-    nvidia.acceptLicense = true;
-    allowUnsupportedSystem = true;
-  };
-  overlays = [ outputs.overlays.packages ];
-};
-
-# Define your hostname.
-networking = {
-  hostName = "nixos-ulad";
-  networkmanager.enable = true;
-};
-
-# Set your time zone.
-time = {
-  hardwareClockInLocalTime = true;
-  timeZone = "Europe/Minsk";
-};
-
-# Select internationalisation properties.
-i18n.defaultLocale = "en_US.UTF-8";
-console = {
-  earlySetup = true;                                  # workaround for https://github.com/NixOS/nixpkgs/issues/257904
-  keyMap = "us";
-};
-
-# Global services configuration
-services = {
-  xserver = {                                         # Environment configuration
-    enable = true;
-    desktopManager = {                                # Dekstop Manager
-      cinnamon.enable = true;
+    kernel.sysctl = {
+      "net.ipv4.ip_default_ttl" = 65; # Sync TTL to mobile
+      "vm.swappiness" = 100;
     };
-    # Language sesttings
-    xkb = {
-      layout = "us,ru";
-      options = "grp:alt_shift_toggle";
+    loader = {
+      systemd-boot = {
+        enable = true; # Systemd-boot loader config
+      };
+      timeout = 10; # Linux boot section timeout
+      efi.canTouchEfiVariables = true;
     };
   };
+  zramSwap = {
+    # Zram support
+    enable = true;
+    memoryPercent = 25;
+    priority = 100;
+  };
 
-  libinput = {                                        # Touchpad  & mouse config
+  # Nix configuration
+  nix = {
+    #package = pkgs.lixPackageSets.latest.lix;
+    channel.enable = false;
+    distributedBuilds = false;
+    buildMachines = [
+      {
+        hostName = "eu.nixbuild.net";
+        system = "x86_64-linux";
+        maxJobs = 100;
+        supportedFeatures = [
+          "benchmark"
+          "big-parallel"
+        ];
+      }
+    ];
+    settings = {
+      trusted-users = [ "@wheel" ];
+      tarball-ttl = 0;
+      builders-use-substitutes = true;
+      substituters = [ "https://nixos-cache-proxy.cofob.dev" ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "auto-allocate-uids"
+      ];
+    };
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      nvidia.acceptLicense = true;
+      allowUnsupportedSystem = true;
+    };
+    overlays = [ outputs.overlays.packages ];
+  };
+
+  # Define your hostname.
+  networking = {
+    hostName = "nixos";
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        IPv6 = {
+          Enabled = true;
+        };
+        Settings = {
+          AutoConnect = true;
+        };
+      };
+    };
+  };
+
+  # Set your time zone.
+  time = {
+    hardwareClockInLocalTime = true;
+    timeZone = "Europe/Minsk";
+  };
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console = {
+    earlySetup = true; # workaround for https://github.com/NixOS/nixpkgs/issues/257904
+    keyMap = "us";
+  };
+
+  # Global services configuration
+  services = {
+    xserver = {
+      # Environment configuration
+      enable = true;
+      desktopManager = {
+        # Dekstop Manager
+        xfce.enable = true;
+      };
+      # Language sesttings
+      xkb = {
+        layout = "us,ru";
+        options = "grp:alt_shift_toggle";
+      };
+    };
+    libinput.enable = true;
+    printing.enable = true;
+    resolved.enable = true;
+  };
+
+  # XDG desktop integration
+  xdg.portal = {
     enable = true;
   };
 
-  # Sound services configuration
-  pipewire = {
-    enable = true;                                      # Pipewire support
-    alsa.enable = true;                                 # Alsa support
-    pulse.enable = true;                                # PulseAudio support
+  # Global hardware configuration
+  hardware = {
+    openrazer.enable = false;
+    bluetooth.enable = true;
+    acpilight.enable = true;
+    i2c.enable = true;
   };
-  printing.enable = true;                             # Printing services
-};
 
-# XDG desktop integration
-xdg.portal = {
-  enable = true;
-};
-
-# Global hardware configuration
-hardware = {
-  openrazer.enable = true;
-  bluetooth.enable = true;
-  acpilight.enable = true;
-  i2c.enable = true;
-};
-
-security = {
-  rtkit.enable = true;
-  polkit.enable = true;
-  sudo.enable = false;
-  doas = {
-    enable = true;
-    extraRules = [{
-      users = [ "ulad" ];
-      keepEnv = true;
-      persist = true;
-    }];
+  security = {
+    rtkit.enable = true;
+    polkit.enable = true;
+    sudo.enable = false;
+    doas = {
+      enable = true;
+      extraRules = [
+        {
+          users = [ "ulad" ];
+          keepEnv = true;
+          persist = true;
+        }
+      ];
+    };
   };
-};
 
-users = {
-  # Declarative configuration for users
-  mutableUsers = false;
-
-  # Current user
   users = {
-    ulad = {
-      isNormalUser = true;
-      description = "Ulad";
-      group = "users";
+    # Declarative configuration for users
+    mutableUsers = false;
+
+    # Current user
+    users = {
+      ulad = {
+        isNormalUser = true;
+        description = "Ulad";
+        group = "users";
         extraGroups = [
           "wheel"
           "adbusers"
@@ -150,14 +175,23 @@ users = {
           "transmission"
           "storage"
           "rslsync"
+          "i2c"
         ];
-      hashedPassword = "$y$j9T$saJvjo68.BgDGPQjA9WDN.$h9979vNxQrblxIxudoFl1qb8twwAMEM4uEbVJ0qCY19";
+        hashedPassword = "$y$j9T$saJvjo68.BgDGPQjA9WDN.$h9979vNxQrblxIxudoFl1qb8twwAMEM4uEbVJ0qCY19";
+      };
     };
   };
-};
 
-system = {
-  stateVersion = "24.05";
-};
+  systemd.user.tmpfiles.rules = [
+    "d /persist/home/ulad/.local/share/Trash 0755 ulad users -"
+  ];
 
+  environment.sessionVariables = {
+    XDG_DATA_HOME = "/persist/home/ulad/.local/share";
+  };
+
+  system = {
+    stateVersion = "24.11";
+    rebuild.enableNg = false;
+  };
 }
