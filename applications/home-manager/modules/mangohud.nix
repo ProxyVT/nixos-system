9611d23d781659932e29916e88b8a9dad0cc70f1
ProxyVT <tikit.us@outlook.com>
2025-08-15 12:20:54 +0300

    Nixfmt style

--- applications/home-manager/modules/mangohud.nix
+++ applications/home-manager/modules/mangohud.nix
@@ -1,22 +1,23 @@
-{ ... }: {
+{ ... }:
+{
 
-programs.mangohud = {
-  enable = true;
-  settingsPerApplication = {
-    mpv = {
+  programs.mangohud = {
+    enable = true;
+    settingsPerApplication = {
+      mpv = {
+        full = true;
+        no_display = true;
+        fps_sampling_period = 1000;
+        fps_limit = 0;
+        fps_limit_method = "late";
+      };
+    };
+    enableSessionWide = true;
+    settings = {
       full = true;
       no_display = true;
       fps_sampling_period = 1000;
-      fps_limit = 0;
-      fps_limit_method = "late";
     };
   };
-  enableSessionWide = true;
-  settings = {
-    full = true;
-    no_display = true;
-    fps_sampling_period = 1000;
-  };
-};
 
 }
