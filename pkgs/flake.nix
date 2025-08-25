968635516300095b5e45b9b8badd04ff9a52f83e
ProxyVT <tikit.us@outlook.com>
2025-08-25 15:15:41 +0300

    Add vivaldi to pkgs testing; add support for unfree builds

--- pkgs/flake.nix
+++ pkgs/flake.nix
@@ -9,12 +9,19 @@
     { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          nvidia.acceptLicense = true;
+        };
+      };
     in
     {
       packages.x86_64-linux = {
         xfce4-alsa-plugin = pkgs.xfce.callPackage ./xfce4-alsa-plugin/default.nix { };
         qmplay2 = pkgs.callPackage ./qmplay2/package.nix { };
+        vivaldi = pkgs.callPackage ./vivaldi/package.nix { };
       };
     };
 }
