34090914f8c268f8b6cb32e8fa2631642a316611
ProxyVT <tikit.us@outlook.com>
2025-09-05 10:57:32 +0300

    Nixfmt style

--- applications/system-manager/modules/transmission.nix
+++ applications/system-manager/modules/transmission.nix
@@ -1,14 +1,13 @@
-{ pkgs, ... }: {
-
-services.transmission = {
-  enable = true;
-  package = pkgs.transmission_4;
-  performanceNetParameters = true;
-  settings = {
-    umask = 0;
-    utp-enabled = true;
-    incomplete-dir-enabled = false;
+{ pkgs, ... }:
+{
+  services.transmission = {
+    enable = true;
+    package = pkgs.transmission_4;
+    performanceNetParameters = true;
+    settings = {
+      umask = 0;
+      utp-enabled = true;
+      incomplete-dir-enabled = false;
+    };
   };
-};
-
 }
