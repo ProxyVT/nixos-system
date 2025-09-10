f07c907c09b975fc57aa3ffc66cd31193a196bb0
ProxyVT <tikit.us@outlook.com>
2025-09-10 21:44:16 +0300

    Add support for all systems via flake-utils

--- pkgs/flake.nix
+++ pkgs/flake.nix
@@ -3,27 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-          nvidia.acceptLicense = true;
-        };
-      };
-    in
     {
-      packages.x86_64-linux = {
-        xfce4-alsa-plugin = pkgs.xfce.callPackage ./xfce4-alsa-plugin/default.nix { };
-        qmplay2 = pkgs.callPackage ./qmplay2/package.nix { };
-        vivaldi = pkgs.callPackage ./vivaldi/package.nix { };
-        ffmpeg-progress-yield = pkgs.python3Packages.callPackage ./ffmpeg-progress-yield/default.nix { };
-        ariang = pkgs.callPackage ./ariang/package.nix { };
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            nvidia.acceptLicense = true;
+          };
+        };
+      in
+      {
+        packages = {
+          xfce4-alsa-plugin = pkgs.xfce.callPackage ./xfce4-alsa-plugin/default.nix { };
+          qmplay2 = pkgs.callPackage ./qmplay2/package.nix { };
+          vivaldi = pkgs.callPackage ./vivaldi/package.nix { };
+          ffmpeg-progress-yield = pkgs.python3Packages.callPackage ./ffmpeg-progress-yield/default.nix { };
+          ariang = pkgs.callPackage ./ariang/package.nix { };
+        };
+      }
+    );
 }
