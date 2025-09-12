927f23de87f7fc91c811f5afbf0aeb25c566f81c
ProxyVT <tikit.us@outlook.com>
2025-09-12 17:04:18 +0300

    Refactor overlay
    Removed repeated code into 'defaultConfig'.

--- applications/system-manager/overlays/default.nix
+++ applications/system-manager/overlays/default.nix
@@ -1,44 +1,31 @@
 { inputs, ... }:
+let
+  defaultConfig = {
+    allowUnfree = true;
+    nvidia.acceptLicense = true;
+  };
+in
 {
-
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
   packages = final: _prev: {
     edge = import inputs.nixpkgs-edge {
       system = final.system;
-      config = {
-        allowUnfree = true;
-        nvidia.acceptLicense = true;
-        allowUnsupportedSystem = true;
-      };
+      config = defaultConfig;
     };
     testing = import inputs.nixpkgs-testing {
       system = final.system;
-      config = {
-        allowUnfree = true;
-        nvidia.acceptLicense = true;
-      };
+      config = defaultConfig;
     };
     release = import inputs.nixpkgs-release {
       system = final.system;
-      config = {
-        allowUnfree = true;
-        nvidia.acceptLicense = true;
-      };
+      config = defaultConfig;
     };
     bird = import inputs.nixpkgs-betterbird {
       system = final.system;
-      config = {
-        allowUnfree = true;
-        nvidia.acceptLicense = true;
-      };
+      config = defaultConfig;
     };
     skype = import inputs.nixpkgs-skype {
       system = final.system;
-      config = {
-        allowUnfree = true;
-        nvidia.acceptLicense = true;
-      };
+      config = defaultConfig;
     };
   };
 
