83c7ac46894efba0ccd7668a8bf69be3bcecbd5d
ProxyVT <tikit.us@outlook.com>
2025-09-12 17:41:45 +0300

    Remove nh overlay
    4.2.0 finally released so i switched back to normal package (it takes too long to build from source with overlay).

--- applications/system-manager/modules/nh.nix
+++ applications/system-manager/modules/nh.nix
@@ -1,24 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.nh = {
     enable = true;
     flake = "/home/ulad/nixos-system";
-    package = pkgs.nh-git;
-  };
-  nixpkgs = {
-    overlays = [
-      (final: prev: {
-        nh-git = prev.nh.overrideAttrs (oldAttrs: rec {
-          version = "4.2.0-beta5";
-          src = oldAttrs.src.override {
-            hash = "sha256-DvonqSRDrGDZmsRCCBegFWtGvb+7SktKn5Rzxgngw+4=";
-          };
-          cargoDeps = final.rustPlatform.fetchCargoVendor {
-            inherit src;
-            hash = "sha256-cQlOOZwqaKae16EWOykftioNVn5zwCAG6fwX+Bg/8rA=";
-          };
-        });
-      })
-    ];
   };
 }
