0ae4eb356c5ccf93eb8d9932ee35a33f5829d7e9
ProxyVT <tikit.us@outlook.com>
2025-09-22 17:38:30 +0300

    Update nvidia.nix

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,12 +1,11 @@
 { config, ... }:
 {
-
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.latest;
     powerManagement.enable = true;
+    modesetting.enable = true;
     open = true;
   };
-
 }
