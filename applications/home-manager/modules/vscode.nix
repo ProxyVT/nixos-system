c90ec79c19c175744f7531b20f8d133c459d0663
ProxyVT <tikit.us@outlook.com>
2025-10-01 15:52:01 +0300

    Switch to home-manager vscode module
    Fixed icon logo for me.

---
+++ applications/home-manager/modules/vscode.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  programs.vscode = {
+    enable = true;
+    package = pkgs.kiro;
+    profiles.default = {
+      userSettings = {
+        "kiroAgent.agentModelSelection" = "claude-sonnet-4";
+        "kiroAgent.enableTabAutocomplete" = true;
+      };
+      extensions = with pkgs.nix-vscode-extensions.vscode-marketplace; [
+        bbenoist.nix
+        jnoortheen.nix-ide
+      ];
+      enableUpdateCheck = false;
+    };
+  };
+
+  # Persist cache for saving loging info between reboots
+  home.persistence."/persist" = {
+    directories = [
+      ".aws"
+    ];
+  };
+}
