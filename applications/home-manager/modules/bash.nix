30eb63f856bd6b2bf502d5d579aa7e1337b38638
ProxyVT <tikit.us@outlook.com>
2025-10-01 15:54:47 +0300

    Add nh aliases for bash
    Much faster than manually input nh os etc.

--- applications/home-manager/modules/bash.nix
+++ applications/home-manager/modules/bash.nix
@@ -8,6 +8,9 @@
       boot = "run0 nixos-rebuild boot --flake";
       switch = "run0 nixos-rebuild switch --flake";
       build = "nixos-rebuild build --flake";
+      nh-boot = "nh os boot --diff always --ask --keep-going --hostname";
+      nh-switch = "nh os switch --diff always --ask --keep-going --hostname";
+      nh-build = "nh os build --diff always --keep-going --hostname";
     };
   };
 }
