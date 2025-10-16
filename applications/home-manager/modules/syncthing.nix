a4c1d76f282d27c93adbe878062105ccf0886db0
ProxyVT <tikit.us@outlook.com>
2025-10-16 16:19:40 +0300

    Update syncthing.nix

--- applications/home-manager/modules/syncthing.nix
+++ applications/home-manager/modules/syncthing.nix
@@ -1,8 +1,17 @@
 { pkgs, ... }:
+let
+  syncthing = pkgs.syncthing.overrideAttrs (oldAttrs: rec {
+    version = "2.0.10";
+    src = oldAttrs.src.override {
+      tag = "v${version}";
+      hash = "sha256-N0+i5sj/cTPDv6q428b3Y0hsPRxIl96+RIuS1AyeTbc=";
+    };
+  });
+in
 {
   services.syncthing = {
     enable = true;
-    package = pkgs.testing.syncthing;
+    package = syncthing;
     overrideDevices = false;
     overrideFolders = false;
     settings.options = {
