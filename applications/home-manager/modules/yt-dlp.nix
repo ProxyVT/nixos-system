b936077f7b6f50c4e5ca81a2687382cfcbca72fe
ProxyVT <tikit.us@outlook.com>
2025-10-16 09:21:37 +0300

    Add yt-dlp-premium version`
    Modern yt-dlp can't download m3u8, which include premium versions of youtube videos.

--- applications/home-manager/modules/yt-dlp.nix
+++ applications/home-manager/modules/yt-dlp.nix
@@ -1,6 +1,21 @@
 { pkgs, ... }:
+let
+  yt-dlp-premium = pkgs.yt-dlp.overrideAttrs rec {
+    pname = "yt-dlp-premium";
+    version = "2025.06.21.224056";
+    src = pkgs.fetchFromGitHub {
+      owner = "coletdjnz";
+      repo = "yt-dlp-dev";
+      tag = version;
+      hash = "sha256-BzxkaA59cQdM7UaU3erM8X4DDPkGRy/lPiAq0BCHfYE=";
+    };
+  };
+  yt-dlp-wrapper = pkgs.writeShellScriptBin "yt-dlp-premium" ''
+    exec ${yt-dlp-premium}/bin/yt-dlp "$@"
+  '';
+in
 {
-
+  home.packages = [ yt-dlp-wrapper ];
   programs.yt-dlp = {
     enable = true;
     package = pkgs.yt-dlp_git;
@@ -10,5 +25,4 @@
       mtime = true;
     };
   };
-
 }
