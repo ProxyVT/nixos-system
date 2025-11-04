0238b607c84e7af6794a64c509ebd7f00069d0f5
ProxyVT <tikit.us@outlook.com>
2025-11-04 10:49:15 +0300

    Update wezterm.nix

--- applications/home-manager/modules/wezterm.nix
+++ applications/home-manager/modules/wezterm.nix
@@ -4,7 +4,13 @@
     enable = true;
     extraConfig = ''
       return {
-        enable_scroll_bar=true
+        enable_scroll_bar = true,
+        freetype_load_flags = 'NO_HINTING',
+        font = wezterm.font_with_fallback({
+        'BigBlueTermPlus Nerd Font Mono',
+        'JetBrains Mono',
+        'Noto Color Emoji'
+        })
       }
     '';
   };
