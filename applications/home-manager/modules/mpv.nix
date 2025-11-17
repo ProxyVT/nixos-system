2218c5b452dcc15685713d63eec26fd4b4bb17fc
ProxyVT <tikit.us@outlook.com>
2025-11-17 17:32:23 +0300

    Update mpv.nix
    Tweak some parameters for consistency with override for syncthing.

--- applications/home-manager/modules/mpv.nix
+++ applications/home-manager/modules/mpv.nix
@@ -12,25 +12,29 @@ let
     mpv =
       (pkgs.mpv-unwrapped.override {
         ffmpeg = pkgs.ffmpeg_8-full;
-        libplacebo = pkgs.libplacebo.overrideAttrs (oldAttrs: {
-          patches = [ ];
-          version = "git";
-          src = oldAttrs.src.override {
-            rev = "06992a53fafc1549843c8b36751e166cfe3a4079";
-            hash = "sha256-CQnE7Olg+YdbU3KfudpZWSEDup06rEiaqA7WPj9Jr/c=";
-          };
-        });
+        libplacebo = pkgs.libplacebo.overrideAttrs (
+          finalAttrs: previousAttrs: {
+            patches = [ ];
+            version = "2e5a392b7f1e4c25d5a3f931e253d71ab566757f";
+            src = previousAttrs.src.override {
+              rev = finalAttrs.version;
+              hash = "sha256-QpxCv2Gt989fp3Qyw6lszC40hUoxipw+hZq+unfyq3s=";
+            };
+          }
+        );
       }).overrideAttrs
-        (oldAttrs: {
-          mesonFlags = lib.lists.filter (flag: !lib.strings.hasPrefix "-Dsdl2" flag) oldAttrs.mesonFlags;
-          patches = [ ];
-          version = "git";
-          src = pkgs.fetchFromGitHub {
-            inherit (oldAttrs.src) owner repo;
-            rev = "2e5e2938dd3f367b73fb58276208aa616e5d37a0";
-            hash = "sha256-hynMuJmVzoExXfYEztXd1QOAsUt/Po+OLQ6PFykd7xk=";
-          };
-        });
+        (
+          finalAttrs: previousAttrs: {
+            mesonFlags = lib.lists.filter (flag: !lib.strings.hasPrefix "-Dsdl2" flag) previousAttrs.mesonFlags;
+            patches = [ ];
+            version = "2e5e2938dd3f367b73fb58276208aa616e5d37a0";
+            src = pkgs.fetchFromGitHub {
+              inherit (previousAttrs.src) owner repo;
+              rev = finalAttrs.version;
+              hash = "sha256-hynMuJmVzoExXfYEztXd1QOAsUt/Po+OLQ6PFykd7xk=";
+            };
+          }
+        );
   };
 in
 {
