234d2354d3341cd3b8361ecaf79a2a75de470aee
ProxyVT <tikit.us@outlook.com>
2025-12-28 15:55:23 +0300

    Update mpv.nix

--- applications/home-manager/modules/mpv.nix
+++ applications/home-manager/modules/mpv.nix
@@ -16,8 +16,8 @@ let
           finalAttrs: previousAttrs: {
             patches = [ ];
             src = previousAttrs.src.override {
-              rev = "47587a2d47299f7261a01b77e989b74f80a99769";
-              hash = "sha256-5VpUEEitsgLGO+P5XxECurfR01XwThkYC685h2iaNgQ=";
+              rev = "bc90ef94944a3dcaab324b86d3e3769ad1d8698b";
+              hash = "sha256-D+2MhG8NyAPUgt5w8CLlirT/kLuUtUT8dN4r5SccBoY=";
             };
           }
         );
@@ -29,8 +29,8 @@ let
             version = "0.41.0";
             src = pkgs.fetchFromGitHub {
               inherit (previousAttrs.src) owner repo;
-              rev = "c0433f064e1eaceeb01602b81174ed96ff6f8b51";
-              hash = "sha256-o2K9eUkcHdOj266SKjrTmJ0jlOL7MuiojvvQoCwBGpM=";
+              rev = "c0d989c83913fb9c990d961c44da4816b8d2de18";
+              hash = "sha256-Gl6UxE9XbFqCjPaz3uLRpSNQF4eZ9wTre0sd4d7HW5g=";
             };
           }
         );
