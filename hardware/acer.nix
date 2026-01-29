2307821ed2aa368983b4b223d870ecf3096f7328
ProxyVT <tikit.us@outlook.com>
2026-01-29 15:27:03 +0300

    Additional tweaks for acer hardware config
    - Removed intel graphics packages
    - Added full composition for vsync

--- hardware/acer.nix
+++ hardware/acer.nix
@@ -12,19 +12,12 @@
       open = false;
       powerManagement.enable = true;
       package = config.boot.kernelPackages.nvidiaPackages.latest;
+      forceFullCompositionPipeline = true;
       prime = {
         sync.enable = true;
         nvidiaBusId = "PCI:4:0:0";
         intelBusId = "PCI:0:2:0";
       };
     };
-    graphics = {
-      extraPackages = [ pkgs.intel-vaapi-driver ];
-      extraPackages32 = [ pkgs.driversi686Linux.intel-vaapi-driver ];
-    };
-  };
-
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "i965";
   };
 }
