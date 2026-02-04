bb5b4992b15435223b5601e0235939df67d9f472
ProxyVT <tikit.us@outlook.com>
2026-02-04 15:36:17 +0300

    Update mpv.nix

--- applications/home-manager/modules/mpv.nix
+++ applications/home-manager/modules/mpv.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  lib,
   ...
 }:
 let
@@ -14,6 +13,7 @@ let
         ffmpeg = pkgs.ffmpeg_8-full;
         libplacebo = pkgs.libplacebo.overrideAttrs (
           finalAttrs: previousAttrs: {
+            version = "v7.358+data=2026-01-13";
             patches = [ ];
             src = previousAttrs.src.override {
               rev = "1dcaea8b601aa969ffd5bfa70088957ce3eaa273";
@@ -24,12 +24,13 @@ let
       }).overrideAttrs
         (
           finalAttrs: previousAttrs: {
+            nativeInstallCheckInputs = [ ];
             patches = [ ];
-            version = "0.41.0";
+            version = "v0.41.0+date=2026-02-02";
             src = pkgs.fetchFromGitHub {
               inherit (previousAttrs.src) owner repo;
-              rev = "d1743b641b5b8a51115a9828124c7a9b527115e3";
-              hash = "sha256-hNx1NtQd/DQr8jmPTnbibLimb4yWAcQqNCJpncEBH1E=";
+              rev = "db22a4528ff4ca77253c0dbc80fb96908c8c48f1";
+              hash = "sha256-u1UyDWBIOBAzy4nYr/5hKzL8t4Gvy4+W1VzbTnRGCTc=";
             };
           }
         );
