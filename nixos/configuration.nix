66adf40c7b0198ce3096c840621ddff0f060a326
ProxyVT <tikit.us@outlook.com>
2026-02-11 12:12:03 +0300

    Switch to default wpa wi-fi backend instead of iwd
    When I used iwd backend it worked poorly with MacBook and Steam Deck, so I decided to go back to standard backend provided by networkmanager.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -92,18 +92,6 @@
     hostName = "nixos";
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd";
-    };
-    wireless.iwd = {
-      enable = true;
-      settings = {
-        IPv6 = {
-          Enabled = true;
-        };
-        Settings = {
-          AutoConnect = true;
-        };
-      };
     };
   };
 
