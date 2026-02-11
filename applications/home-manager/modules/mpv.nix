bf9dad6b21e8fc0f002add467ca91982ff58f6b9
ProxyVT <tikit.us@outlook.com>
2026-02-11 16:08:07 +0300

    Update mpv.nix

--- applications/home-manager/modules/mpv.nix
+++ applications/home-manager/modules/mpv.nix
@@ -13,11 +13,11 @@ let
         ffmpeg = pkgs.ffmpeg_8-full;
         libplacebo = pkgs.libplacebo.overrideAttrs (
           finalAttrs: previousAttrs: {
-            version = "v7.358+data=2026-01-13";
+            version = "v7.358+data=2026-02-09";
             patches = [ ];
             src = previousAttrs.src.override {
-              rev = "1dcaea8b601aa969ffd5bfa70088957ce3eaa273";
-              hash = "sha256-lgPawUN0rMcAJU8eaPfFPSoJ8xH20+gUlZewNslabIs=";
+              rev = "b2ea27dceb6418aabfe9121174c6dbb232942998";
+              hash = "sha256-0d1coxOMDh5/Xi0dcI5rMgQVZojVE5j9Wt9LmsES3SM=";
             };
           }
         );
@@ -26,11 +26,11 @@ let
           finalAttrs: previousAttrs: {
             nativeInstallCheckInputs = [ ];
             patches = [ ];
-            version = "v0.41.0+date=2026-02-02";
+            version = "v0.41.0+date=2026-02-10";
             src = pkgs.fetchFromGitHub {
               inherit (previousAttrs.src) owner repo;
-              rev = "db22a4528ff4ca77253c0dbc80fb96908c8c48f1";
-              hash = "sha256-u1UyDWBIOBAzy4nYr/5hKzL8t4Gvy4+W1VzbTnRGCTc=";
+              rev = "3847a8157f6e44d7788dea2d1c172bc20bc50120";
+              hash = "sha256-9fRzlxdOh1Na5h7CuNMUTQNVH9DZazC8kCFffQVFwus=";
             };
           }
         );
