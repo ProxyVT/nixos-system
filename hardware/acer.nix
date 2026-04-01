aeb46baa1c7c9c5164f5b56fb6c89bea58fff17e
ProxyVT <tikit.us@outlook.com>
2026-04-01 16:53:14 +0300

    Switch to legacy 580 drivers
    My gt 940m become legacy gpu, so nixpkgs team finally added alias for 580 as legacy.

--- hardware/acer.nix
+++ hardware/acer.nix
@@ -11,8 +11,7 @@
     nvidia = {
       open = false;
       powerManagement.enable = true;
-      package = config.boot.kernelPackages.nvidiaPackages.latest;
-      forceFullCompositionPipeline = true;
+      package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
       prime = {
         sync.enable = true;
         nvidiaBusId = "PCI:4:0:0";
