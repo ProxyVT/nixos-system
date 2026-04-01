28a70b3babbfc481667c5b8ccd3644b39daa8982
ProxyVT <tikit.us@outlook.com>
2026-04-01 16:49:42 +0300

    Update firefox.nix

--- applications/home-manager/modules/firefox.nix
+++ applications/home-manager/modules/firefox.nix
@@ -12,6 +12,8 @@
         "browser.tabs.closeWindowWithLastTab" = false;
         "browser.translations.automaticallyPopup" = false;
         "browser.tabs.groups.smart.enabled" = true;
+        "browser.sessionstore.restore_pinned_tabs_on_demand" = true;
+        "browser.settings-redesign.enabled" = true;
         "dom.webgpu.enabled" = true;
         "gfx.webrender.all" = true;
       };
