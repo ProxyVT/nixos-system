ac115af3a87121fb7e3a99b9cae97713cf7ae895
ProxyVT <tikit.us@outlook.com>
2026-06-27 21:16:10 +0300

    Remove BigBlueTermPlus font
    It doesn't look like I wanted, so I removing it for now.

--- applications/home-manager/modules/wezterm.nix
+++ applications/home-manager/modules/wezterm.nix
@@ -7,7 +7,6 @@
         enable_scroll_bar = true,
         freetype_load_flags = 'NO_HINTING',
         font = wezterm.font_with_fallback({
-        'BigBlueTermPlus Nerd Font Mono',
         'JetBrains Mono',
         'Noto Color Emoji'
         })
