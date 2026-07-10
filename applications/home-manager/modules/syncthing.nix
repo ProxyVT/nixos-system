4d1945b5f073413575adea52cecfbdc5b5536d67
ProxyVT <tikit.us@outlook.com>
2026-07-10 23:07:47 +0300

    Update syncthing.nix

--- applications/home-manager/modules/syncthing.nix
+++ applications/home-manager/modules/syncthing.nix
@@ -2,7 +2,7 @@
 let
   syncthing-git = pkgs.syncthing.overrideAttrs (
     finalAttrs: previousAttrs: {
-      version = "2.1.2-rc.4";
+      version = "2.1.2";
       src = previousAttrs.src.override {
         tag = "v${finalAttrs.version}";
         hash = "sha256-tR4yXoEYbCi8Uh3wCyATc+J4+cGvD7k0d9egjHS5H4k=";
