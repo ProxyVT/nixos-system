a1130cc4b10f9e5953e0fc428d47d4ae2043588c
ProxyVT <tikit.us@outlook.com>
2026-08-03 11:51:18 +0300

    Update release to 26.05
    I made it long time ago, just forgot to push changes.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Personal flake configuration";
 
   inputs = {
-    nixpkgs-release.url = "https://channels.nixos.org/nixos-25.11/nixexprs.tar.xz";
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs-release.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.xz";
     nixpkgs-edge.url = "github:nixos/nixpkgs/master";
     nixpkgs-testing.url = "github:ProxyVT/nixpkgs/testing";
     impermanence.url = "github:nix-community/impermanence";
