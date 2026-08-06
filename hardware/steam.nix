f0d8e34d39008cb68fc5824d47fbd78ea1c0e9b3
ProxyVT <tikit.us@outlook.com>
2026-08-07 02:27:00 +0300

    Update steam.nix

--- hardware/steam.nix
+++ hardware/steam.nix
@@ -1,17 +1,20 @@
-{ lib, ... }:
+{ inputs, lib, pkgs, ... }:
 {
+  nixpkgs.overlays = [ inputs.steam-deck-overlay.overlays.default ];
   jovian = {
     devices.steamdeck = {
       enableVendorDrivers = true;
       enableOsFanControl = true;
       enableDefaultCmdlineConfig = true;
       enableDefaultStage1Modules = true;
+      enableSoundSupport = true;
     };
     steamos = {
       enableSysctlConfig = true;
       enableHdmiCecIntegration = true;
       enableDefaultCmdlineConfig = true;
     };
+    hardware.has.amd.gpu = true;
   };
-  services.scx.enable = lib.mkForce false;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_jovian;
 }
