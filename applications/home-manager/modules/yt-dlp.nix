ca0d2ce3d2de7b7186afad0e0a6f63aa07e0b569
ProxyVT <tikit.us@outlook.com>
2026-08-18 12:44:46 +0300

    Update yt-dlp.nix

--- applications/home-manager/modules/yt-dlp.nix
+++ applications/home-manager/modules/yt-dlp.nix
@@ -2,17 +2,9 @@
 {
   programs.yt-dlp = {
     enable = true;
-    package = pkgs.testing.yt-dlp.overrideAttrs (old: {
-      version = "sabr";
-      src = pkgs.fetchFromGitHub {
-        owner = "bashonly";
-        repo = "yt-dlp";
-        rev = "sabr";
-        hash = "sha256-m39e5sb7QCMfezq11M0LwR4irPVvbXxQNNavHdALodw=";
-      };
-    });
+    package = pkgs.yt-dlp_git;
     settings = {
-      downloader = "aria2c";
+      downloader = "wget";
       merge-output-format = "mkv";
       mtime = true;
       extractor-args = "youtube:player-client=visionos";
