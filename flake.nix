0ae64a94a16d9292485c8e4b14455b32f94f6320
ProxyVT <tikit.us@outlook.com>
2026-08-30 01:53:32 +0300

    Remove inputs follows + alphabetical order

--- flake.nix
+++ flake.nix
@@ -2,53 +2,32 @@
   description = "Personal flake configuration";
 
   inputs = {
-    determinate.url = "https://flakehub.com/f/DeterminateSystems/nix-src/*";
-    nixpkgs.url = "https://channels.nixos.org/nixos-unstable-small/nixexprs.tar.xz";
-    nixpkgs-testing.url = "github:ProxyVT/nixpkgs/testing";
-    multiverse.url = "github:fzakaria/nixpkgs-multiverse";
+    agenix.url = "github:ryantm/agenix";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/nix-src/*";
+    home-manager.url = "github:nix-community/home-manager";
     impermanence.url = "github:nix-community/impermanence";
+    multios-usb.url = "github:Mexit/MultiOS-USB";
+    multiverse.url = "github:fzakaria/nixpkgs-multiverse";
+    nh.url = "github:nix-community/nh";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
-    nix-vscode-extensions = {
-      url = "github:nix-community/nix-vscode-extensions";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    xlibre-overlay = {
-      url = "git+https://codeberg.org/takagemacoed/xlibre-overlay?ref=dev-26.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    multios-usb = {
-      url = "github:Mexit/MultiOS-USB";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nh = {
-      url = "github:nix-community/nh";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    picom = {
-      url = "github:yshui/picom";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    nixpkgs-testing.url = "github:ProxyVT/nixpkgs/testing";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    picom.url = "github:yshui/picom";
+    xlibre-overlay.url = "git+https://codeberg.org/takagemacoed/xlibre-overlay?ref=dev-26.11";
   };
 
   outputs =
     {
-      self,
-      nixpkgs,
+      agenix,
       chaotic,
+      home-manager,
       impermanence,
       nix-flatpak,
-      home-manager,
+      nixpkgs,
+      self,
       xlibre-overlay,
-      agenix,
       ...
     }@inputs:
 
@@ -60,15 +39,15 @@
       defaultModules = [
         ./nixos
         ./applications/system-manager
-        home-manager.nixosModules.default
-        chaotic.nixosModules.default
-        jovian.nixosModules.default
-        impermanence.nixosModules.default
-        nix-flatpak.nixosModules.nix-flatpak
-        xlibre-overlay.nixosModules.overlay-xlibre-xserver
-        xlibre-overlay.nixosModules.overlay-all-xlibre-drivers
-        xlibre-overlay.nixosModules.overlay-xpra
         agenix.nixosModules.default
+        chaotic.nixosModules.default
+        home-manager.nixosModules.default
+        impermanence.nixosModules.default
+        jovian.nixosModules.default
+        nix-flatpak.nixosModules.nix-flatpak
+        xlibre-overlay.nixosModules.overlay-all-xlibre-drivers
+        xlibre-overlay.nixosModules.overlay-xlibre-xserver
+        xlibre-overlay.nixosModules.overlay-xpra
       ];
 
       mkNixosConfig =
@@ -83,11 +62,11 @@
     {
       custom-packages = import ./applications/system-manager/overlays { inherit inputs system; };
       nixosConfigurations = {
-        nixos = mkNixosConfig { hardwareFile = ./hardware/default.nix; };
-        steam = mkNixosConfig { hardwareFile = ./hardware/steam.nix; };
         acer = mkNixosConfig { hardwareFile = ./hardware/acer.nix; };
-        umka = mkNixosConfig { hardwareFile = ./hardware/umka.nix; };
+        nixos = mkNixosConfig { hardwareFile = ./hardware/default.nix; };
         nvidia = mkNixosConfig { hardwareFile = ./hardware/nvidia.nix; };
+        steam = mkNixosConfig { hardwareFile = ./hardware/steam.nix; };
+        umka = mkNixosConfig { hardwareFile = ./hardware/umka.nix; };
       };
     };
 }
