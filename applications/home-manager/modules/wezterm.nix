eea029a0a181803799dbec28fae5c0ca303512a0
ProxyVT <tikit.us@outlook.com>
2026-09-03 01:05:47 +0300

    Update wezterm.nix

--- applications/home-manager/modules/wezterm.nix
+++ applications/home-manager/modules/wezterm.nix
@@ -4,6 +4,7 @@
     enable = true;
     extraConfig = ''
       return {
+        scrollback_lines = 9999,
         enable_scroll_bar = true,
         freetype_load_flags = 'NO_HINTING',
         font = wezterm.font_with_fallback({
