7618ba357024f51e19640c4d8a868208f7e7842e
ProxyVT <tikit.us@outlook.com>
2026-09-03 01:04:35 +0300

    Update mpv.nix

--- applications/home-manager/modules/mpv.nix
+++ applications/home-manager/modules/mpv.nix
@@ -6,10 +6,10 @@
 let
   uosc = pkgs.mpvScripts.uosc.overrideAttrs (
     finalAttrs: previousAttrs: {
-      version = "2026-08-03";
+      version = "2026-08-30";
       src = previousAttrs.src.override {
-        rev = "d124c2c930d69446448022851373e00ae592390d";
-        hash = "sha256-5fHihGI2rodEByqTRs3NasmLUBjG3VY9l/YnKDCKSt8=";
+        rev = "12b918fcbcae56ded0e073a965d769bb0c5d900e";
+        hash = "sha256-T1zHFhjU3DHd/CRQGr1XVSo2duj1rjP6JZqAFdaLaPw=";
       };
     }
   );
@@ -20,16 +20,27 @@ let
     ];
     mpv-unwrapped =
       (pkgs.mpv-unwrapped.override {
-        ffmpeg = pkgs.ffmpeg_8;
+        ffmpeg = pkgs.ffmpeg.overrideAttrs (
+          finalAttrs: previousAttrs: {
+            doCheck = false;
+            version = "2026-09-02";
+            src = pkgs.fetchFromGitHub {
+              owner = "FFmpeg";
+              repo = "FFmpeg";
+              rev = "9fc8c785e2747c87121ec28f8f10ceab0562384b";
+              hash = "sha256-9UUxp6fRri8nQf1+VBGh7jVDFBS2VCP5n4Z2rpAv+mo=";
+            };
+          }
+        );
         libplacebo = pkgs.libplacebo.overrideAttrs (
           finalAttrs: previousAttrs: {
-            version = "2026-08-12";
+            version = "2026-09-01";
             patches = [ ];
             src = pkgs.fetchFromGitLab {
               inherit (previousAttrs.src) owner repo;
               domain = "code.videolan.org";
-              rev = "22ee762e8e0890fc54068beb670310f0edce7263";
-              hash = "sha256-RLGEjMVhPUoBh0OmHKzO8NJTREcYkWdAKA0foX00Bos=";
+              rev = "86bbd5df43fe579d0f173c538a94584ab79d7e10";
+              hash = "sha256-hNkkH5yKymSXWJJXSXyzNkfo59aSib0nwsCTzehtYQU=";
             };
           }
         );
@@ -51,11 +62,11 @@ let
                 popd
               ''
             ];
-            version = "2026-08-13";
+            version = "2026-09-02";
             src = pkgs.fetchFromGitHub {
               inherit (previousAttrs.src) owner repo;
-              rev = "7b8915bc1d04c7e1b61184e00c7fbfaab1911e75";
-              hash = "sha256-pY32N/FoUyKIvz9GuJg21AU4CjRAKPhYatQID/1SZrM=";
+              rev = "69e63f425a531f814431fba12750bdb3721357f2";
+              hash = "sha256-hiuLY7tobXA4F0/iCnEqbMeD1qKROc37HyMR5pKY6g4=";
             };
           }
         );
