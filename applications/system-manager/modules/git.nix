2a6df221c65c8a6883961d5e6492e94074b72ba1
ProxyVT <tikit.us@outlook.com>
2026-09-08 01:06:43 +0300

    Update git.nix

--- applications/system-manager/modules/git.nix
+++ applications/system-manager/modules/git.nix
@@ -13,6 +13,10 @@
           name = "ProxyVT";
           email = "tikit.us@outlook.com";
         };
+        url = {
+          "ssh://git@github.com/".insteadOf = "https://github.com/";
+          "ssh://git@gitlab.com/".insteadOf = "https://gitlab.com/";
+        };
         safe.directory = [ "/home/ulad/*" ];
       };
     };
